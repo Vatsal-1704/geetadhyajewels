1a4f57a69f253b86f2618be6a799e25d2762869c
Vatsal-1704 <vatsal1704@gmail.com>
2026-06-01 14:45:03 +0200

    Update API URL to production Render backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api" });
+// const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api" });
+const api = axios.create({ baseURL: "https://geetadhyajewels-api.onrender.com/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("gjToken");
